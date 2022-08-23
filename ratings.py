7bd1d8b9c33a0c8f8e44c8f7aa3bf6802a567888
suelenk <suelenmatosr@gmail.com>
2022-08-23 15:28:53 -0400

    splited the code in different functions

--- ratings.py
+++ ratings.py
@@ -14,27 +14,7 @@ def restaurantes_rates(file_name):
 
     ratings = {}
 
-    with open(file_name) as restaurante_ratings:         
-
-        restaurante_from_user = input("Enter a Restaurante name: ").title()       
-        
-
-        while True:
-            restaurante_rate_from_user = input("Enter a rate between 1 and 5 for this restaurante: ")
-                        
-            if restaurante_rate_from_user.isdigit() != True:
-                print("not valid Rate")
-                continue    
-
-            restaurante_rate_from_user_int = int(restaurante_rate_from_user)
-
-            if restaurante_rate_from_user_int <= 0 or restaurante_rate_from_user_int > 5:
-                print("not valid Rate: ")
-                continue  
-            
-            break  
-
-        ratings[restaurante_from_user] = restaurante_rate_from_user
+    with open(file_name) as restaurante_ratings:     
 
         for line in restaurante_ratings.readlines():
             restaurante = line.replace("\n" , "")
@@ -42,14 +22,46 @@ def restaurantes_rates(file_name):
             restaurante_name = restaurante_list[0]
             restaurante_rates = restaurante_list[1]        
             ratings[restaurante_name] = restaurante_rates
+   
+    return ratings
+
+
+def restaurante_from_user_to_dict(restaurante_ratings):
+
+        restaurante_from_user = input("Enter a Restaurante name: ").title()          
+
+        while True:
+            restaurante_rating_from_user = input("Enter a rate between 1 and 5 for this restaurante: ")
+                        
+            if restaurante_rating_from_user.isdigit() != True:
+                print("not valid Rate")
+                continue    
+
+            restaurante_rating_from_user_int = int(restaurante_rating_from_user)
+
+            if restaurante_rating_from_user_int <= 0 or restaurante_rating_from_user_int > 5:
+                print("not valid Rate: ")
+                continue  
+            
+            break  
+
+        restaurante_ratings[restaurante_from_user] = restaurante_rating_from_user
+
+
+
+def print_ratings(ratings):
 
     sort_dict = ratings.items()
     new_sort_dict = sorted(sort_dict)
+    print(*new_sort_dict, sep="\n")   
 
-    # print(*new_sort_dict, sep="\n")
-    return new_sort_dict
 
-rates_from_restaurante = restaurantes_rates("scores.txt")
-print(*rates_from_restaurante, sep="\n")    
 
+
+
+
+ratings_for_restaurantes = restaurantes_rates("scores.txt")
+restaurante_from_user_to_dict(ratings_for_restaurantes)
+print_ratings(ratings_for_restaurantes)
+ 
 
